56156f38d294dced4e012fd6c5852c4ccc9ca177
Thomas, G (Geert-Jan) <Geert-Jan.Thomas@rabobank.com>
2025-12-04 15:44:46 +0100

    Sync from source repo on 2025-12-04 15:44:39

---
+++ LocalDeploy/Forms/Instructions.cs
@@ -0,0 +1,29 @@
+﻿namespace LocalDeploy.Forms;
+
+public partial class Instructions : Form
+{
+    public Instructions()
+    {
+        InitializeComponent();
+    }
+
+    private void Instructions_Load(object sender, EventArgs e)
+    {
+        var assemblyFolder = new FileInfo(GetType().Assembly.Location).DirectoryName;
+
+        var pathToInstructionsFile = Path.Join(assemblyFolder, "FirstTimeInstructions.mht");
+
+        webBrowser.Navigate($"file://{pathToInstructionsFile}");
+
+    }
+
+    protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+    {
+        if (keyData == Keys.Escape)
+        {
+            Close();
+            return true;
+        }
+        return base.ProcessCmdKey(ref msg, keyData);
+    }
+}
